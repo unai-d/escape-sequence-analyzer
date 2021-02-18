b0dcd703a2bd60fcf28ea380483798879f8877fb
Unai Dominguez <unai55@outlook.es>
2021-02-18 14:06:14 +0100

    First commit

---
+++ Enums.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EscapeSequenceAnalyzer
+{
+    enum ESCAPE_TYPE
+    {
+        NONE = 0x00,
+        // Fe
+        PADDING_CHARACTER = 0x80,
+        SINGLE_SHIFT_TWO = 0x78,            // 'N'
+        SINGLE_SHIFT_THREE = 0x79,          // 'O'
+        DEVICE_CONTROL_STRING = 0x7a,       // 'P'
+        CONTROL_SEQUENCE_INTRODUCER = 0x5b, // '['
+        STRING_TERMINATOR = 0x5c,           // '\'
+        OPERATING_SYSTEM_COMMAND = 0x5d,    // ']'
+        START_OF_STRING = 0x98,
+        PRIVACY_MESSAGE = 0x9e,
+        APPLICATION_PROGRAM_COMMAND = 0x9f
+    }
+
+    enum OSC_CTRL
+    {
+        NONE = 0x00,
+        SET_TITLE = 0x30,
+        HYPERLINK = 0x38,
+    }
+
+    enum CSI_CTRL
+    {
+        NONE = 0x00,
+        CURSOR_UP = (byte)'A',
+        CURSOR_DOWN = (byte)'B',
+        CURSOR_FORWARD = (byte)'C',
+        CURSOR_BACK = (byte)'D',
+        CURSOR_POSITION = (byte)'H',
+        ERASE_IN_DISPLAY = (byte)'J',
+        ERASE_IN_LINE = (byte)'K',
+        SCROLL_UP = (byte)'S',
+        SCROLL_DOWN = (byte)'T',
+        SELECT_GRAPHIC_RENDITION = (byte)'m',
+        ENABLE = (byte)'h',
+        DISABLE = (byte)'l',
+    }
+
+    enum CSI_SELECT_GRAPHIC_RENDITION_CODE
+    {
+        UNKNOWN = -1,
+
+        RESET = 0,
+        BOLD = 1,
+        FAINT = 2,
+        ITALIC = 3,
+        UNDERLINED = 4,
+        BLINK = 5,
+        FAST_BLINK = 6,
+        INVERSE = 7,
+        INVISIBLE = 8,
+        STRIKE = 9,
+
+        DEFAULT_FONT = 10,
+        ALTERNATIVE_FONT_1 = 11,
+        ALTERNATIVE_FONT_2 = 12,
+        ALTERNATIVE_FONT_3 = 13,
+        ALTERNATIVE_FONT_4 = 14,
+        ALTERNATIVE_FONT_5 = 15,
+        ALTERNATIVE_FONT_6 = 16,
+        ALTERNATIVE_FONT_7 = 17,
+        ALTERNATIVE_FONT_8 = 18,
+        ALTERNATIVE_FONT_9 = 19,
+
+        BLACKLETTER_FONT = 20,
+        DOUBLE_UNDERLINE = 21,
+        DISABLE_BOLD_AND_FAINT = 22,
+        DISABLE_ITALIC_AND_BLACKLETTER = 23,
+        NO_UNDERLINE = 24,
+        NO_BLINK = 25,
+        PROPORTIONAL_SPACING = 26,
+        NOT_INVERSED = 27,
+        NOT_INVISIBLE = 28,
+        DISABLE_STRIKE = 29,
+
+        FG_BLACK = 30,
+        FG_RED = 31,
+        FG_GREEN = 32,
+        FG_YELLOW = 33,
+        FG_BLUE = 34,
+        FG_MAGENTA = 35,
+        FG_CYAN = 36,
+        FG_WHITE = 37,
+        FG_8BIT = 38,
+        FG_DEFAULT = 39,
+
+        BG_BLACK = 40,
+        BG_RED = 41,
+        BG_GREEN = 42,
+        BG_YELLOW = 43,
+        BG_BLUE = 44,
+        BG_MAGENTA = 45,
+        BG_CYAN = 46,
+        BG_WHITE = 47,
+        BG_8BIT = 48,
+        BG_DEFAULT = 49,
+
+        DISABLE_PROPORTIONAL_SPACING = 50,
+        FRAMED = 51,
+        ENCIRCLED = 52,
+        OVERLINED = 53,
+        NEITHER_FRAMED_NOR_ENCIRCLED = 54,
+        NOT_OVERLINED = 55,
+        SET_UNDERLINE_COLOR = 58,
+        DEFAULT_UNDERLINE_COLOR = 59,
+
+        SUPERSCRIPT = 73,
+        SUBSCRIPT = 74,
+
+        FG_GRAY = 90,
+        FG_BRIGHT_RED = 91,
+        FG_BRIGHT_GREEN = 92,
+        FG_BRIGHT_YELLOW = 93,
+        FG_BRIGHT_BLUE = 94,
+        FG_BRIGHT_MAGENTA = 95,
+        FG_BRIGHT_CYAN = 96,
+        FG_BRIGHT_WHITE = 97,
+
+        BG_GRAY = 100,
+        BG_BRIGHT_RED = 101,
+        BG_BRIGHT_GREEN = 102,
+        BG_BRIGHT_YELLOW = 103,
+        BG_BRIGHT_BLUE = 104,
+        BG_BRIGHT_MAGENTA = 105,
+        BG_BRIGHT_CYAN = 106,
+        BG_BRIGHT_WHITE = 107,
+    }
+
+    enum CSI_ERASE_IN_DISPLAY_CODE
+	{
+        FROM_CURSOR_TO_END_OF_SCREEN = 0,
+        FROM_CURSOR_TO_BEGINNING_OF_SCREEN = 1,
+        ALL_SCREEN = 2,
+        ALL_SCREEN_AND_SCROLLBACK_BUFFER = 3
+	}
+
+    enum CSI_ERASE_IN_LINE_CODE
+	{
+        FROM_CURSOR_TO_END_OF_LINE = 0,
+        FROM_CURSOR_TO_BEGINNING_OF_LINE = 1,
+        ALL_LINE = 2,
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Threading;
+
+namespace EscapeSequenceAnalyzer
+{
+    class Program
+    {
+        static bool DecodingEscapeSequence = false;
+        
+        static ESCAPE_TYPE EscapeType = ESCAPE_TYPE.NONE;
+        static OSC_CTRL CurrentOscCtrl = OSC_CTRL.NONE;
+
+        static void Main(string[] args)
+        {
+            var stdin = Console.OpenStandardInput();
+            string[] escargs = new string[8];
+
+            int pos = 0, charcount = 0, argsindex = 0, retries = 0;
+
+            while (retries < 5)
+            {
+                int b = stdin.ReadByte();
+                if (b == -1) { Thread.Sleep(200); retries++; continue; }
+
+                if (!DecodingEscapeSequence) // Text mode
+                {
+                    if (b != 0x1b) // Not entering escape mode
+                    {
+                        Console.WriteLine($"0x{pos:X4}  {b:X2}.");
+                        charcount++;
+                    }
+                    else // Entering escape mode
+                    {
+                        Console.WriteLine($"0x{pos:X4}  1B ESC.");
+                        DecodingEscapeSequence = true;
+                        charcount = 0;
+                    }
+                }
+                else // Escape mode
+                {
+                    bool terminate = false;
+
+                    if (EscapeType == ESCAPE_TYPE.NONE) // Esc mode not set.
+                    {
+                        EscapeType = (ESCAPE_TYPE)b;
+                        Console.WriteLine($"0x{pos:X4}  {b:X2} {EscapeType}.");
+                    }
+                    else if (EscapeType == ESCAPE_TYPE.OPERATING_SYSTEM_COMMAND) // Esc mode is OSC ]
+                    {
+                        if (b == 0x07)
+                        {
+                            terminate = true;
+                        }
+                        else if (Enum.IsDefined(typeof(OSC_CTRL), b))
+                        {
+                            CurrentOscCtrl = (OSC_CTRL)b;
+                            Console.WriteLine($"0x{pos:X4}  {b:X2} {CurrentOscCtrl}.");
+                        }
+                        else
+                        {
+                            escargs[argsindex] += (char)b;
+                            Console.WriteLine($"0x{pos:X4}  {b:X2} '{(char)b}'.");
+                        }
+                    }
+                    else if (EscapeType == ESCAPE_TYPE.STRING_TERMINATOR) // Esc mode is ST \
+                    {
+                        terminate = true;
+                    }
+                    else if (EscapeType == ESCAPE_TYPE.CONTROL_SEQUENCE_INTRODUCER) // Esc mode is CSI [
+                    {
+                        if (b >= 0x30 & b <= 0x39) // 0-9
+                        {
+                            escargs[argsindex] += (char)b;
+                            Console.WriteLine($"0x{pos:X4}  {b:X2} NUMBER_{(char)b}.");
+                        }
+                        else if (b == 0x3a) // ':'
+                        {
+                            argsindex++;
+                            Console.WriteLine($"0x{pos:X4}  3A COLON_DELIMITER.");
+                        }
+                        else if (b == 0x3b) // ';'
+                        {
+                            argsindex++;
+                            Console.WriteLine($"0x{pos:X4}  3B SEMICOLON_DELIMITER.");
+                        }
+                        else if (b == 0x3f) // '?'
+                        {
+                            escargs[argsindex] += (char)b;
+                            Console.WriteLine($"0x{pos:X4}  3F PRIVATE_CONTROL_SEQUENCE.");
+                        }
+                        else if (Enum.IsDefined(typeof(CSI_CTRL), b))
+                        {
+                            Console.WriteLine($"0x{pos:X4}  {b:X2} {(CSI_CTRL)b}.");
+                            switch ((CSI_CTRL)b)
+                            { 
+                                case CSI_CTRL.SELECT_GRAPHIC_RENDITION:
+                                    foreach (string s in escargs)
+                                    {
+                                        if (s != null && s != "")
+                                            Console.WriteLine($"           '{s}' = {(CSI_SELECT_GRAPHIC_RENDITION_CODE)Convert.ToInt32(s)}.");
+                                    }
+                                    break;
+
+                                case CSI_CTRL.ERASE_IN_DISPLAY:
+                                    Console.WriteLine($"           {(CSI_ERASE_IN_DISPLAY_CODE)Convert.ToInt32(escargs[0])}.");
+                                    break;
+
+                                case CSI_CTRL.ERASE_IN_LINE:
+                                    Console.WriteLine($"           {(CSI_ERASE_IN_LINE_CODE)Convert.ToInt32(escargs[0])}.");
+                                    break;
+
+                                case CSI_CTRL.CURSOR_POSITION:
+                                    Console.WriteLine($"           ROW {escargs[0] ?? "1"}, COLUMN {escargs[1] ?? "1"}.");
+                                    break;
+
+                                case CSI_CTRL.SCROLL_UP:
+                                case CSI_CTRL.SCROLL_DOWN:
+                                    Console.WriteLine($"           {escargs[0] ?? "1"} LINES.");
+                                    break;
+
+                                case CSI_CTRL.ENABLE:
+                                case CSI_CTRL.DISABLE:
+                                    if (escargs[0] == "?25")
+                                    {
+                                        Console.WriteLine($"           CURSOR.");
+                                    }
+                                    else if (escargs[0] == "?1049")
+									{
+                                        Console.WriteLine($"           ALTERNATIVE_SCREEN_BUFFER.");
+                                    }
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"0x{pos:X4}  {b:X2} UND.");
+                        }
+
+                        if (b >= 0x40 && b <= 0x7e) // Entering text mode (terminator of CSI)
+                        {
+                            terminate = true;
+                        }
+                    }
+                    else // Unrecognized Fe mode.
+                    {
+                        Console.WriteLine($"0x{pos:X4}  {b:X2} UND.");
+                    }
+
+                    if (terminate)
+                    {
+                        Console.WriteLine($"           ESC_END.");
+                        DecodingEscapeSequence = false;
+                        EscapeType = ESCAPE_TYPE.NONE;
+                        escargs = new string[8];
+                        argsindex = 0;
+                    }
+                }
+
+                pos++;
+            }
+        }
+    }
+}
